21e4fec9ba3a0f8d444fd91e6a604622b6e95e9f
kamuthuj <kamuthuj@gmail.com>
2025-04-28 17:21:45 +0300

    Added minimum and maximum values to the input features

--- app.py
+++ app.py
@@ -17,13 +17,20 @@ st.title("🌾Crop Recommendation System")
 st.markdown("Enter the soil and environmental conditions below to get a crop recommendation:")
 
 # Input fields
-N = st.number_input("Nitrogen (N)", min_value=0.0)
-P = st.number_input("Phosphorus (P)", min_value=0.0)
-K = st.number_input("Potassium (K)", min_value=0.0)
-temperature = st.number_input("Temperature (°C)", format="%.2f")
-humidity = st.number_input("Humidity", format="%.2f")
-PH = st.number_input("Soil pH", format="%.2f")
-rainfall = st.number_input("Rainfall (mm)", format="%.2f")
+st.markdown("*Nitrogen Min value 0 and max value 180*")
+N = st.number_input("Nitrogen (N)", min_value=0.0,max_value=180.0)
+st.markdown("*Phosphorus (P) Min value 5 and max value 150*")
+P = st.number_input("Phosphorus (P)", min_value=5.0,max_value=150.0)
+st.markdown("*Potassium (K) Min value 5 and max value 220*")
+K = st.number_input("Potassium (K)", min_value=5.0,max_value=220.0)
+st.markdown("*Temperature (°C) Min value 7 and max value 45*")
+temperature = st.number_input("Temperature (°C)", min_value=7.0,max_value=45.0)
+st.markdown("*Humidity Min value 10 and max value 120*")
+humidity = st.number_input("Humidity", min_value=10.0,max_value=120.0)
+st.markdown("*Soil pH Min value 3.5 and max value 10*")
+PH = st.number_input("Soil pH", min_value=3.5,max_value=10.0)
+st.markdown("*Rainfall Min value 20 and max value 300*")
+rainfall = st.number_input("Rainfall (mm)", min_value=20.0,max_value=300.0)
 
 # Predict button
 if st.button("Predict Crop"):
